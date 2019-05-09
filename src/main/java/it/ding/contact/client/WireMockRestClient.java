44c71cf7c90af9846f7236294e87362d7ad6db5f
Kwo Ding <kwo@ding.it>
2019-05-09 20:56:17 +0200

    Implement configurable base uris

--- src/main/java/it/ding/contact/client/WireMockRestClient.java
+++ src/main/java/it/ding/contact/client/WireMockRestClient.java
@@ -1,15 +1,18 @@
 package it.ding.contact.client;
 
 import static io.restassured.http.ContentType.JSON;
+import static it.ding.contact.data.CommonData.PROPERTY_WIREMOCK_BASE_URI;
 import static java.util.Objects.requireNonNull;
 import static org.apache.http.HttpStatus.SC_CREATED;
 import static org.apache.http.HttpStatus.SC_OK;
 
+import it.ding.contact.util.GlobalProperties;
 import java.io.File;
 
 public class WireMockRestClient extends RestClient {
 
-    private static final String BASE_URI = "http://localhost:8080";
+    private static final GlobalProperties globalProperties = GlobalProperties.getInstance();
+    private static final String BASE_URI = globalProperties.getString(PROPERTY_WIREMOCK_BASE_URI);
     private static final String ADMIN_URI = "/__admin";
     private static final String MAPPINGS = ADMIN_URI + "/mappings";
     private static final String RESET = ADMIN_URI + "/reset";
