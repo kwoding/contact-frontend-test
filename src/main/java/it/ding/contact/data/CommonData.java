38b42acd9059c15d8943c3012158c6975f74b61d
Kwo Ding <kwo@ding.it>
2019-05-09 21:04:24 +0200

    Implement configurable base uris

---
+++ src/main/java/it/ding/contact/data/CommonData.java
@@ -0,0 +1,11 @@
+package it.ding.contact.data;
+
+public class CommonData {
+
+    private CommonData() {
+    }
+
+    public static final String PROPERTY_APP_BASE_URI = "app.base.uri";
+
+    public static final String PROPERTY_WIREMOCK_BASE_URI = "wiremock.base.uri";
+}
