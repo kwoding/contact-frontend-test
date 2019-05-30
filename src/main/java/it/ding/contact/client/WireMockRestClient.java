f2a296e4ffe5b88437efb9a675d01b6c36aee814
Kwo Ding <kwo@ding.it>
2019-05-30 16:31:05 +0200

    Add request journal methods

--- src/main/java/it/ding/contact/client/WireMockRestClient.java
+++ src/main/java/it/ding/contact/client/WireMockRestClient.java
@@ -8,6 +8,8 @@ import static org.apache.http.HttpStatus.SC_OK;
 
 import it.ding.contact.util.GlobalProperties;
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WireMockRestClient extends RestClient {
 
@@ -16,6 +18,8 @@ public class WireMockRestClient extends RestClient {
     private static final String ADMIN_URI = "/__admin";
     private static final String MAPPINGS = ADMIN_URI + "/mappings";
     private static final String RESET = ADMIN_URI + "/reset";
+    private static final String REQUESTS_RESET = ADMIN_URI + "/requests/reset";
+    private static final String REQUESTS_FIND = ADMIN_URI + "/requests/find";
 
     public WireMockRestClient() {
         super(BASE_URI);
@@ -41,5 +45,30 @@ public class WireMockRestClient extends RestClient {
             .post(RESET)
             .then()
             .statusCode(SC_OK);
+    }
+
+    public void resetRequestJournal() {
+        requestSpec()
+            .contentType(JSON)
+            .post(REQUESTS_RESET)
+            .then()
+            .statusCode(SC_OK);
+    }
+
+    public String findFirstRequestBody(String method, String urlPathPattern) {
+        Map<String, String> requestBody = new HashMap<>();
+
+        requestBody.put("method", method);
+        requestBody.put("urlPathPattern", urlPathPattern);
+
+        return requestSpec()
+            .contentType(JSON)
+            .body(requestBody)
+            .post(REQUESTS_FIND)
+            .then()
+            .statusCode(SC_OK)
+            .extract()
+            .jsonPath()
+            .getString("requests[0].body");
     }
 }
